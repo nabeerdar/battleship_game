999c643adb435998df39e02519177b3b5f6e6c40
nabeerdar <52133097+nabeerdar@users.noreply.github.com>
2024-09-15 20:58:39 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-// index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Home';
-import Battleship from './Battleship';
-import Map from './Map';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/battleship" element={<Battleship />} />
-      <Route path="/map" element={<Map />} />
-    </Routes>
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
