1e54b64e8735649af8c1ade76934d9fbcb8a4b5e
nabeerdar <52133097+nabeerdar@users.noreply.github.com>
2024-09-15 20:58:18 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import Home from './Home';
+import Battleship from './Battleship';
+import Map from './Map';
+import './index.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+
+function App() {
+    return (
+      
+      <>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/battleship" element={<Battleship />} />
+            <Route path="/map" element={<Map />} />
+          </Routes>
+        </Router>
+      </>
+      
+    );
+}
+export default App;
